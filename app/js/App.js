5f32355e5e8ef72d05882c3fcdc0719eaab77385
Roman <roman.cherniy89@gmail.com>
2018-01-26 00:39:11 +0200

    add projects

---
+++ app/js/App.js
@@ -0,0 +1,18 @@
+function App() {
+	this.init();
+}
+
+
+
+
+App.prototype = Object.create(Helper.prototype);
+App.prototype.init = function () {
+  new Mmenu();
+}
+
+window.addEventListener( "DOMContentLoaded", function() {
+	new App();
+
+
+	slider();
+});
---
+++ app/js/components/slider-inner.js
@@ -0,0 +1,8 @@
+
+function slider() {
+    $('.slider-inner').slick({
+      infinite: true,
+      prevArrow:'<button class="slick-prev slick-arrow icon-left-open" aria-label="Previous" type="button"></button>',
+      nextArrow:'<button class="slick-next slick-arrow icon-right-open" aria-label="Next" type="button"></button>'
+    });
+}
